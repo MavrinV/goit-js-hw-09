6777dc1a1a252d4d111333356eee6ffb4feaf727
MavrinV <167757992+MavrinV@users.noreply.github.com>
2025-03-21 09:25:48 +0000

    Deploying to gh-pages from @ MavrinV/goit-js-hw-09@572698226da0ddf795632e18ca922071caca30d8 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=document.querySelector(".feedback-form"),s="feedback-form-state";let e={email:"",message:""};const m=localStorage.getItem(s);m&&(e=JSON.parse(m),t.elements.email.value=e.email,t.elements.message.value=e.message);t.addEventListener("input",a=>{e[a.target.name]=a.target.value,localStorage.setItem(s,JSON.stringify(e))});t.addEventListener("submit",a=>{a.preventDefault(),!(!e.email||!e.message)&&(console.log(e),localStorage.removeItem(s),e={email:"",message:""},t.reset())});
+//# sourceMappingURL=2-form.js.map
